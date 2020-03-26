ec16de86cc1dbc4a6c59583a196edd1ff21866db
Ugur Gül <ugur.gul@platri.de>
2020-03-26 11:37:14 +0100

    typeorm package is implemented

--- flatten-backend/src/app.module.ts
+++ flatten-backend/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {QrCodeModule} from "./qr-code/qr-code.module";
+import { QrCodeModule } from "./qr-code/qr-code.module";
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [QrCodeModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    QrCodeModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
