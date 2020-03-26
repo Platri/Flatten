3b878c83f860994cc7542fe425051210e4a0ec1f
DritonUkic <DritonUkic@users.noreply.github.com>
2020-03-26 13:09:13 +0100

    Revert "update typeorm type"
    This reverts commit 8deb9a35ddc1995eebc102e70b27a1788e4ecba4.

--- flatten-backend/src/app.module.ts
+++ flatten-backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { QrCodeModule } from "./qr-code/qr-code.module";
 import { HealthLogbookModule } from './health-logbook/health-logbook.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
+//import { TypeOrmModule } from '@nestjs/typeorm';
+/*
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -20,7 +21,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     QrCodeModule,
     HealthLogbookModule
   ],
+
+import {QrCodeModule} from "./qr-code/qr-code.module"; */
+
+@Module({
+  imports: [QrCodeModule, HealthLogbookModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
