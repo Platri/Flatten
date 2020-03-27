8d10235e04bc6a29dc1e2e4b7671244c7c6bb3ae
Tim Deisser <tim.deisser@gmx.de>
2020-03-27 08:22:24 +0100

    dockerize app and use databse config service

--- flatten-backend/src/app.module.ts
+++ flatten-backend/src/app.module.ts
@@ -1,37 +1,22 @@
+import { DatabaseConfigService } from './config/database-config.service';
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { QrCodeModule } from './qr-code/qr-code.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { HealthLogbookModule } from '@src/health-logbook/health-logbook.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    QrCodeModule,
-    HealthLogbookModule,
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('TYPEORM_HOST'),
-        port: configService.get<number>('TYPEORM_PORT'),
-        username: configService.get<string>('TYPEORM_USERNAME'),
-        password: configService.get<string>('TYPEORM_PASSWORD'),
-        database: configService.get<string>('TYPEORM_DATABASE'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        migrations: [__dirname + '/migration/**/*{.ts,.js}'],
-        migrationsTableName: 'migration',
-        cli: {
-          entitiesDir: 'src/entity',
-          migrationsDir: 'src/migration',
-        },
-        synchronize: false,
-      }),
-      inject: [ConfigService],
-    }),
-  ],
-  controllers: [],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		QrCodeModule,
+		HealthLogbookModule,
+		TypeOrmModule.forRootAsync({
+			imports: [ ConfigModule ],
+			useClass: DatabaseConfigService
+		})
+	],
+	controllers: [],
+	providers: [ DatabaseConfigService, AppService ]
 })
 export class AppModule {}
---
+++ flatten-backend/src/config/database-config.service.ts
@@ -0,0 +1,27 @@
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Injectable()
+export class DatabaseConfigService implements TypeOrmOptionsFactory {
+	constructor(private readonly configService: ConfigService) {}
+
+	createTypeOrmOptions(): TypeOrmModuleOptions {
+		return {
+			type: 'mysql',
+			host: this.configService.get<string>('TYPEORM_HOST'),
+			port: this.configService.get<number>('TYPEORM_PORT'),
+			username: this.configService.get<string>('TYPEORM_USERNAME'),
+			password: this.configService.get<string>('TYPEORM_PASSWORD'),
+			database: this.configService.get<string>('TYPEORM_DATABASE'),
+			entities: [ __dirname + '/**/*.entity{.ts,.js}' ],
+			migrations: [ __dirname + '/migration/**/*{.ts,.js}' ],
+			migrationsTableName: 'migration',
+			cli: {
+				entitiesDir: 'src/entity',
+				migrationsDir: 'src/migration'
+			},
+			synchronize: false
+		};
+	}
+}
