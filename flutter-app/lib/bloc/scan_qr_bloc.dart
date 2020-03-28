936c3c43dfb4e9cf5d95a3152ea52acc859a2653
Oscar Reyes <oscar.e.reyes.r@gmail.com>
2020-03-28 13:32:01 +0100

    Basic Scanning Functionality

--- flutter-app/lib/bloc/scan_qr_bloc.dart
+++ flutter-app/lib/bloc/scan_qr_bloc.dart
@@ -4,23 +4,36 @@ import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:flatten/bloc/bloc_provider.dart';
 import 'package:rxdart/rxdart.dart';
 
+enum QRScannerState{Scanning, ScanSuccess}
+
 class ScanQrBloc implements BlocBase {
 	QRViewController controller;
+	final List<String> scannedData = [];
 
-	final BehaviorSubject<bool> _qrCodeFoundController = BehaviorSubject.seeded(false);
-	Sink<bool> get _inQrCodeFound =>_qrCodeFoundController.sink;
-	Stream<bool> get outQrCodeFound =>_qrCodeFoundController.stream;
+	final BehaviorSubject<QRScannerState> _qrScannerStateController = BehaviorSubject.seeded(QRScannerState.Scanning);
+	Sink<QRScannerState> get _inQRScannerState =>_qrScannerStateController.sink;
+	Stream<QRScannerState> get outQRScannerState =>_qrScannerStateController.stream;
 
-	void _handleIncomingQrValue(String qrValue) => _inQrCodeFound.add(true);
-
-	void onQRViewCreated(QRViewController controller) {
+	void onQRViewCreated(QRViewController controller) async {
 		this.controller = controller;
-		controller.scannedDataStream.listen((scanData) => _inQrCodeFound.add(true));
+		controller.scannedDataStream.listen((scanData) => handleNewScannedData(scanData));
 	}
+
+	void handleNewScannedData(String data) async {
+		if (!scannedData.contains(data)){
+			scannedData.add(data);
+			_inQRScannerState.add(QRScannerState.ScanSuccess);
+			await sleep();
+			_inQRScannerState.add(QRScannerState.Scanning);
+		}
+
+	}
+
+	Future sleep() => Future.delayed(const Duration(seconds: 3), () => "3");
 
 	@override
 	void dispose() {
 		controller?.dispose();
-		_qrCodeFoundController.close();
+		_qrScannerStateController.close();
 	}
 }
