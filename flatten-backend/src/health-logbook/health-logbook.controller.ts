0b27b88817c39af2c8ec081a80b607c5ecb2c0fa
Ugur Gül <ugur.gul@platri.de>
2020-03-29 10:41:45 +0200

    response documentation for health-logbook is impelemted

--- flatten-backend/src/health-logbook/health-logbook.controller.ts
+++ flatten-backend/src/health-logbook/health-logbook.controller.ts
@@ -1,11 +1,10 @@
-import {Body, Controller, Get, Param, Post, Query} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Query, Res} from '@nestjs/common';
 import { HealthLogbookService } from '@src/health-logbook/health-logbook.service';
 import {
   HealthLogbookEntry
 } from '@src/health-logbook/healthLogbook.model';
-import { ApiTags } from '@nestjs/swagger';
-import {CreateHealthLogbookEntryDto, GetHealthLogbookListDto} from "@src/shared/dtos/healthLogbook.dto";
-import {HealthLogbook} from "@src/entity/health-logbook.entity";
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
+import {CreateHealthLogbookEntryDto, GetHealthLogbookListDto, ResponseHealthLogbookDTO} from "@src/shared/dtos/healthLogbook.dto";
 import {PaginationDto} from "@src/shared/dtos/pagination.dto";
 
 @Controller('health-logbook')
@@ -13,24 +12,21 @@ import {PaginationDto} from "@src/shared/dtos/pagination.dto";
 export class HealthLogbookController {
   constructor(private healthLogbookService: HealthLogbookService) {}
 
-
   @Get(':qrId')
   async getLogbook(@Param('qrId') qrId: string, @Query('page') page = 0,  @Query('pageSize') pageSize = 0  ): Promise<PaginationDto<GetHealthLogbookListDto>> {
-    console.log("abcd");
     return await this.healthLogbookService.getLogbook(qrId, page, pageSize);
   }
 
-
   @Get()
+  @ApiResponse({status: 200, description: 'The found record', type: ResponseHealthLogbookDTO})
   async getAll(): Promise<HealthLogbookEntry[]> {
-    console.log("cd");
     return await this.healthLogbookService.findAll();
   }
 
   @Post()
   async createLogbookEntry(
-      @Body() dto: CreateHealthLogbookEntryDto,
-  ): Promise<HealthLogbookEntry> {
+    @Body() dto: CreateHealthLogbookEntryDto,
+    ): Promise<HealthLogbookEntry> {
     return await this.healthLogbookService.createLogbookEntry(dto);
   }
 }
