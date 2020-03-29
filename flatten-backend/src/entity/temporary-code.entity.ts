3556783205b56c93c8f706bf601949c3ba789b30
Serdar Nurgün <serdar.nurguen@platri.de>
2020-03-29 11:53:50 +0200

    Corrected used dto to create temporary code

--- flatten-backend/src/entity/temporary-code.entity.ts
+++ flatten-backend/src/entity/temporary-code.entity.ts
@@ -1,34 +1,40 @@
 import {
-    PrimaryGeneratedColumn,
-    Column,
-    Entity,
-    Index,
-    OneToOne,
-    JoinColumn
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  Index,
+  OneToOne,
+  JoinColumn,
 } from 'typeorm';
-import {Max, Min} from "class-validator";
-import {QRCode} from "@src/entity/qr-code.entity";
+import { Max, Min } from 'class-validator';
+import { QRCode } from '@src/entity/qr-code.entity';
 
 @Entity()
 export class TemporaryCode {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Index({unique: true})
-    @Min(8)
-    @Max(8)
-    @Column({ type: 'varchar', length: 8, default: 'EMPTYVA1', nullable: true})
-    shortCode: string;
+  @Index({ unique: true })
+  @Min(8)
+  @Max(8)
+  @Column({ type: 'varchar', length: 8, default: 'EMPTYVA1', nullable: true })
+  shortCode: string;
 
+  @Column({
+    type: 'timestamp',
+    precision: null,
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createDateTime: Date;
 
-    @Column({ type: 'timestamp', precision: null, default: () => 'CURRENT_TIMESTAMP' })
-    createDateTime: Date;
+  @Column({
+    type: 'timestamp',
+    precision: null,
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  validUntil: Date;
 
-    @Column({ type: 'timestamp', precision: null, default: () => 'CURRENT_TIMESTAMP' })
-    validUntil: Date;
-
-    @OneToOne(type => QRCode)
-    @JoinColumn()
-    qrCode: QRCode;
-
+  @OneToOne(type => QRCode)
+  @JoinColumn()
+  qrCode: QRCode;
 }
--- flatten-backend/src/temporary-code/temporary-code.controller.ts
+++ flatten-backend/src/temporary-code/temporary-code.controller.ts
@@ -2,7 +2,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TemporaryCodeService } from '@src/temporary-code/temporary-code.service';
 import { TemporaryCode } from '@src/entity/temporary-code.entity';
-import { TemporaryCodeDto } from '@src/shared/dtos/temporary-code.dto';
+import { CreateTemporaryCodeDto } from '@src/shared/dtos/create-temporary-code.dto';
 
 @Controller('temporary-codes')
 @ApiTags('temporary-codes')
@@ -15,7 +15,7 @@ export class TemporaryCodeController {
   }
 
   @Post()
-  async create(@Body() dto: TemporaryCodeDto): Promise<TemporaryCode> {
+  async create(@Body() dto: CreateTemporaryCodeDto): Promise<TemporaryCode> {
     return await this.service.createTemporaryCode(dto);
   }
 }
